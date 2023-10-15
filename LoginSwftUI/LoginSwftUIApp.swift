aae0bc0316772cb9b66808365a5fc4fac52d573e
Luis Mendez <dluismendezdj@gmail.com>
2023-10-15 07:05:34 -0400

    Initial Commit

---
+++ LoginSwftUI/LoginSwftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSwftUIApp.swift
+//  LoginSwftUI
+//
+//  Created by Luis Mendez on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginSwftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
