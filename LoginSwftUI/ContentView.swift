860e06fc7d9ec2587ac042b96fd47c4576b29c21
Luis Mendez <dluismendezdj@gmail.com>
2023-10-16 19:35:33 -0400

    Create login screen

--- LoginSwftUI/ContentView.swift
+++ LoginSwftUI/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color
+                    .blue
+                    .ignoresSafeArea()
+                
+                Circle()
+                    .scale(1.8)
+                    .foregroundColor(.white.opacity(0.15))
+                
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                
+                VStack {
+                    Text("Sign In")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+//                    .navigationDestination(isPresented: $showingLoginScreen) {
+//                        Text("Hi, @\(username)")
+//                    }
+                    NavigationLink(destination: Text("Hi, @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }
+            .navigationBarHidden(false)
         }
-        .padding()
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "lmdev" {
+            wrongUsername = 0
+            
+            if password.lowercased() == ".swift.py==big" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
     }
 }
 
